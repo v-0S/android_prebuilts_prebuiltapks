8547ba6b02c0871b866cfa1f79aa1937c525c223
v1nc <github@reckendrees.systems>
2021-09-04 09:17:15 +0200

    fix AuroraServices

--- AuroraServices/Android.mk
+++ AuroraServices/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := AuroraService
+LOCAL_MODULE := AuroraServices
 LOCAL_SRC_FILES := AuroraServices_v1.1.1.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
